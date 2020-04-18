132ced3db344ac9d3f6bbd38d2c915edcdee2a33
acomarcho <acoxstpd@gmail.com>
2020-04-18 10:13:02 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ char baku[][20] = {
     "abjad", "akhirat", "aksesori", "aktif", "akuarium", "aluminium", "ambulans",
     "analisis", "antena", "antre", "anugerah", "apotek", "asas", "asasi", "atlet",
     "atmosfer", "autopsi", "balsam", "batalion", "baterai", "becermin", "besok",
-    "blanko", "boks", "bosan", "bus", "cabai", "capai", "cedera", "cendekiawan", "cengkih",
-    "cinderamata", "cokelat", "daftar", "derajat", "detail", "detergen", "diagnosis",
+    "biseps", "boks", "bosan", "bus", "cabai", "capai", "cedera", "cendekiawan", "cengkih",
+    "cendera mata", "cokelat", "daftar", "derajat", "detail", "detergen", "diagnosis",
     "durian", "efektif", "efektivitas", "ekosistem", "ekspor", "ekstra", "ekstrakurikuler",
     "ekstrem", "elite", "favorit", "februari", "film", "finis", "formal", "foto",
     "fotokopi", "fotosintesis", "frekuensi", "grup", "gua", "gubuk", "hafal", "hakikat",
@@ -29,11 +29,11 @@ char baku[][20] = {
     "menyontek", "menyukseskan", "merek", "meterai", "metode", "miliar", "modern", "mozaik",
     "museum", "nafsu", "nahas", "nakhoda", "napas", "narasumber", "nasihat", "negeri",
     "nomor", "nonaktif", "nonformal", "november", "objek", "objektif", "ojek", "olahraga",
-    "omzet", "otomatis", "paham", "pasif", "paspor", "pembaruan", "penasihat", "perajin",
+    "omzet", "otomatis", "paham", "pasif", "paspor", "pembaruan", "penasihat", "boling",
     "persentase", "persentase", "perusak", "petai", "pikir", "praktik", "prancis", "prangko",
     "proaktif", "provinsi", "proyek", "putra", "putri", "rabu", "ramai", "rapi", "realitas",
     "relatif", "religius", "respons", "restoran", "rezim", "risiko", "ritme", "rezeki",
-    "rontgen", "rubuh", "sahaja", "saksama", "samudra", "sangsi", "saraf", "satai", "sekadar",
+    "rontgen", "rubuh", "sahaja", "saksama", "samudra", "hadis", "saraf", "satai", "sekadar",
     "sekretaris", "selebritas", "sentimeter", "seriawan", "setrika", "siapa pun", "silakan",
     "sistem", "sopir", "standar", "standardisasi", "stres", "stroberi", "studi", "subjek",
     "sumatra", "surga", "survei", "sutra", "syair", "takhayul", "takhta", "tampak", "taoco",
@@ -41,15 +41,15 @@ char baku[][20] = {
     "telur", "tenteram", "tepercaya", "terampil", "terima kasih", "tetapi", "tidak", "tim",
     "tobat", "toleransi", "tradisional", "transpor", "tur", "ubah", "unta", "urine", "utang",
     "variasi", "varietas", "verifikasi", "video", "vila", "volume", "wiraswasta", "yogyakarta",
-    "yudikatif", "zaman"
+    "yudikatif", "zaman", "blangko"
 };
 
 char notBaku[][20] = {
     "abjat", "akherat", "asesoris", "aktip", "aquarium", "almunium", "ambulan",
     "analisa", "antene", "antri", "anugrah", "apotik", "azas", "azasi", "atlit",
     "atmosfir", "otopsi", "balsem", "batalyon", "batere", "bercermin", "esok",
-    "blangko", "bok", "bosen", "bis", "cabe", "capek", "cidera", "cendikiawan", "cengkeh",
-    "cenderamata", "coklat", "daptar", "derajad", "detil", "deterjen", "diagnosa",
+    "bisep", "bok", "bosen", "bis", "cabe", "capek", "cidera", "cendikiawan", "cengkeh",
+    "cinderamata", "coklat", "daptar", "derajad", "detil", "deterjen", "diagnosa",
     "duren", "efektip", "efektifitas", "ekosistim", "eksport", "extra", "ekstrakulikuler",
     "ekstrim", "elit", "pavorit", "pebruari", "filem", "finish", "formil", "photo",
     "photo copy", "fotosintesa", "frekwensi", "group", "goa", "gubug", "hapal", "hakekat",
@@ -65,11 +65,11 @@ char notBaku[][20] = {
     "mencontek", "mensukseskan", "merk", "materai", "metoda", "milyar", "moderen", "mosaik",
     "musium", "napsu", "naas", "nahkoda", "nafas", "nara sumber", "nasehat", "negri",
     "nomer", "non aktif", "non formal", "nopember", "obyek", "obyektif", "ojeg", "olah raga",
-    "omset", "automatis", "faham", "pasive", "pasport", "pembaharuan", "penasehat", "pengrajin",
+    "omset", "automatis", "faham", "pasive", "pasport", "pembaharuan", "penasehat", "bowling",
     "presentase", "prosentase", "pengrusak", "pete", "fikir", "praktek", "perancis", "perangko",
     "pro aktif", "propinsi", "projek", "putera", "puteri", "rebo", "rame", "rapih", "realita",
     "relative", "relijius", "respon", "restauran", "rejim", "resiko", "ritma", "rejeki",
-    "ronsen", "roboh", "sehaja", "seksama", "samudera", "sanksi", "syaraf", "sate", "sekedar",
+    "ronsen", "roboh", "sehaja", "seksama", "samudera", "hadist", "syaraf", "sate", "sekedar",
     "sekertaris", "selebriti", "centimeter", "sariawan", "seterika", "siapapun", "silahkan",
     "sistim", "supir", "standard", "standarisasi", "stress", "strawberi", "study", "subyek",
     "sumatera", "sorga", "survey", "sutera", "sair", "tahayul", "tahta", "nampak", "tauco",
@@ -77,7 +77,7 @@ char notBaku[][20] = {
     "telor", "tentram", "terpercaya", "trampil", "terimakasih", "tapi", "nggak", "team",
     "taubat", "tolerir", "tradisionil", "transport", "tour", "rubah", "onta", "urin", "hutang",
     "vareasi", "varitas", "veripikasi", "vidio", "villa", "volum", "wirausaha", "jogjakarta",
-    "yudikatip", "jaman"
+    "yudikatip", "jaman", "blanko"
 };
 
 int main() {
@@ -104,7 +104,7 @@ int main() {
             break;
         }
         if (!strcmp(ans, baku[qNumber])) {
-            cout << "Jawaban anda benar!" << endl;
+            cout << "Jawaban Anda benar!" << endl;
             qStyle = rand() % 2;
             qNumber = rand() % (sizeof(baku) / sizeof(baku[0]));
 
@@ -117,7 +117,7 @@ int main() {
             }
             continue;
         } else {
-            cout << "Jawaban anda salah!" << endl;
+            cout << "Jawaban Anda salah!" << endl;
             qStyle = rand() % 2;
             qNumber = rand() % (sizeof(baku) / sizeof(baku[0]));
 
